26bc6cd67b458b06e2a11c715973233cb16ad0d8
Fernando G. Albornoz <fgalbornoz@outlook.com>
2020-02-09 18:27:15 -0300

    Verificado ControladorInsumos

--- SyStock/LogicaNegocio/ClasedeDominio/ControladorInsumo.cs
+++ SyStock/LogicaNegocio/ClasedeDominio/ControladorInsumo.cs
@@ -7,34 +7,45 @@ namespace SyStock.LogicaNegocio.ClasedeDominio
 {
     public static class ControladorInsumo
     {
+        /// <summary>
+        /// Permite agregar un Insumo
+        /// </summary>
+        /// <param name="pInsumo">Insumo a agregar</param>
+        /// <returns>-1 si hubo éxito</returns>
         public static int Agregar(Insumo pInsumo)
         {
+            if ((pInsumo == null) || (pInsumo.Nombre.Length == 0))
+                throw new ArgumentNullException(nameof(pInsumo));
+
             DAOFactory factory = DAOFactory.Instancia();
 
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                int idInsumo = _insumoDAO.VerificarNombre(pInsumo.Nombre);
+                int idInsumo = factory.InsumoDAO.VerificarNombre(pInsumo.Nombre);
                 factory.FinalizarConexion();
+
                 if (idInsumo == -1)
                 {
                     factory.IniciarConexion();
-                    _insumoDAO.Agregar(pInsumo);
+                    factory.InsumoDAO.Agregar(pInsumo);
+                    factory.FinalizarConexion();
                 }
                 return idInsumo;
             }
-            catch (DAOException)
+            catch (DAOException e)
             {
                 factory.RollBack();
-                return -2;
-            }
-            finally
-            {
                 factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Permite obtener un Insumo. Buscar por ID
+        /// </summary>
+        /// <param name="pIdInsumo">ID del insumo a buscar</param>
+        /// <returns>Insumo encontrado. Null en caso de error</returns>
         public static Insumo Obtener(int pIdInsumo)
         {
             DAOFactory factory = DAOFactory.Instancia();
@@ -42,21 +53,24 @@ namespace SyStock.LogicaNegocio.ClasedeDominio
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                Insumo _insumo = new Insumo(0, "", "", 0, 0, true, 0);
-                _insumo = _insumoDAO.Obtener(pIdInsumo);
-                return _insumo;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            finally
-            {
+                Insumo insumo = factory.InsumoDAO.Obtener(pIdInsumo);
                 factory.FinalizarConexion();
+
+                return insumo;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Permite obtener un Insumo. Buscar por nombre
+        /// </summary>
+        /// <param name="pNombreInsumo">Nombre del insumo a buscar</param>
+        /// <returns>Insumo encontrado. Null en caso de error</returns>
         public static Insumo Obtener(string pNombreInsumo)
         {
             DAOFactory factory = DAOFactory.Instancia();
@@ -64,86 +78,87 @@ namespace SyStock.LogicaNegocio.ClasedeDominio
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                Insumo _insumo = new Insumo(0, "", "", 0, 0, true, 0);
-                _insumo = _insumoDAO.Obtener(pNombreInsumo);
-                return _insumo;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            finally
-            {
+                Insumo insumo = factory.InsumoDAO.Obtener(pNombreInsumo);
                 factory.FinalizarConexion();
+
+                return insumo;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
-        public static bool Modificar(Insumo pInsumo)
+        /// <summary>
+        /// Permite modificar un Insumo
+        /// </summary>
+        /// <param name="pInsumo">Insumo a modificar. Busca por su ID</param>
+        public static void Modificar(Insumo pInsumo)
         {
             DAOFactory factory = DAOFactory.Instancia();
 
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                _insumoDAO.Modificar(pInsumo);
-                return true;
+                factory.InsumoDAO.Modificar(pInsumo);
+                factory.FinalizarConexion();
             }
-            catch (DAOException)
+            catch (DAOException e)
             {
                 factory.RollBack();
-                return false;
-            }
-            finally
-            {
                 factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Lista todos los insumos
+        /// </summary>
+        /// <returns>Lista conteniendo todos los insumos disponibles</returns>
         public static List<Insumo> Listar()
         {
             DAOFactory factory = DAOFactory.Instancia();
-            List<Insumo> _listaInsumo = new List<Insumo>();
 
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                _listaInsumo = _insumoDAO.Listar();
-                return _listaInsumo;
-            }
-            catch (Exception)
-            {
-                _listaInsumo.Clear();
-                return _listaInsumo;
-            }
-            finally
-            {
+                List<Insumo> listaInsumo = factory.InsumoDAO.Listar();
                 factory.FinalizarConexion();
+
+                return listaInsumo;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Lista todos los insumos que pertenecen a una Categoría
+        /// </summary>
+        /// <param name="idCategoria">ID de la categoría a filtrar</param>
+        /// <returns>Lista conteniendo todos los insumos de una Categoría</returns>
         public static List<Insumo> Listar(int idCategoria)
         {
             DAOFactory factory = DAOFactory.Instancia();
-            List<Insumo> _listaInsumo = new List<Insumo>();
 
             try
             {
                 factory.IniciarConexion();
-                IInsumoDAO _insumoDAO = factory.InsumoDAO;
-                _listaInsumo = _insumoDAO.Listar(idCategoria);
-                return _listaInsumo;
-            }
-            catch (Exception)
-            {
-                _listaInsumo.Clear();
-                return _listaInsumo;
-            }
-            finally
-            {
+                List<Insumo> listaInsumo = factory.InsumoDAO.Listar(idCategoria);
                 factory.FinalizarConexion();
+
+                return listaInsumo;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
     }
