f1d4290e6d992099bc7547473b7c883a7d1c6678
Fernando G. Albornoz <fgalbornoz@outlook.com>
2020-02-09 18:16:51 -0300

    Revisión en ControladorEntrega

--- SyStock/LogicaNegocio/ClasedeDominio/ControladorEntrega.cs
+++ SyStock/LogicaNegocio/ClasedeDominio/ControladorEntrega.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using SyStock.AccesoDatos;
 using SyStock.Entidades;
 
@@ -8,53 +7,63 @@ namespace SyStock.LogicaNegocio.ClasedeDominio
 {
     public static class ControladorEntrega
     {
+        /// <summary>
+        /// Permite agregar una Entrega
+        /// </summary>
+        /// <param name="pEntrega">Entrega a agregar</param>
+        /// <returns>-1 si tuvo éxito</returns>
         public static int Agregar(EntregaInsumos pEntrega)
         {
+            if (pEntrega == null)
+                throw new ArgumentNullException(nameof(pEntrega));
+
             DAOFactory factory = DAOFactory.Instancia();
 
             try
             {
                 factory.IniciarConexion();
-                IEntregaDAO _entregaDAO = factory.EntregaDAO;
-                _entregaDAO.Agregar(pEntrega);
-                List<EntregaInsumos> _list = new List<EntregaInsumos>();
-                _list = _entregaDAO.Listar();
-                return _list.Count();
+                factory.EntregaDAO.Agregar(pEntrega);
+                factory.FinalizarConexion();
+
+                return -1;
             }
-            catch (DAOException)
+            catch (DAOException e)
             {
                 factory.RollBack();
-                return -2;
-            }
-            finally
-            {
                 factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Lista las Entregas existentes
+        /// </summary>
+        /// <returns>Lista con todas las entregas</returns>
         public static List<EntregaInsumos> Listar()
         {
             DAOFactory factory = DAOFactory.Instancia();
-            List<EntregaInsumos> _listaEntrega = new List<EntregaInsumos>();
 
             try
             {
                 factory.IniciarConexion();
-                IEntregaDAO _entregaDAO = factory.EntregaDAO;
-                _listaEntrega = _entregaDAO.Listar();
-                return _listaEntrega;
-            }
-            catch (Exception)
-            {
-                _listaEntrega.Clear();
-                return _listaEntrega;
-            }
-            finally
-            {
+                List<EntregaInsumos> listaEntrega = factory.EntregaDAO.Listar();
                 factory.FinalizarConexion();
+
+                return listaEntrega;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
 
+        /// <summary>
+        /// Permite obtener una Entrega
+        /// </summary>
+        /// <param name="idEntrega">ID de la entrega a buscar</param>
+        /// <returns>Entrega encontrada en caso de éxito. Null caso contrario</returns>
         public static EntregaInsumos Obtener(int idEntrega)
         {
             DAOFactory factory = DAOFactory.Instancia();
@@ -62,18 +71,16 @@ namespace SyStock.LogicaNegocio.ClasedeDominio
             try
             {
                 factory.IniciarConexion();
-                IEntregaDAO _entregaDAO = factory.EntregaDAO;
-                EntregaInsumos _entrega = new EntregaInsumos(0,0,DateTime.Today);
-                _entrega = _entregaDAO.Obtener(idEntrega);
-                return _entrega;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            finally
-            {
+                EntregaInsumos entrega = factory.EntregaDAO.Obtener(idEntrega);
                 factory.FinalizarConexion();
+
+                return entrega;
+            }
+            catch (DAOException e)
+            {
+                factory.RollBack();
+                factory.FinalizarConexion();
+                throw new LogicaException(e.Message);
             }
         }
     }
