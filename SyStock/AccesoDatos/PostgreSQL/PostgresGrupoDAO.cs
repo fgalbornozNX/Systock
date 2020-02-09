dd6196d74d77f76226ab218a639a65b25ad56d05
Fernando G. Albornoz <fgalbornoz@outlook.com>
2020-02-09 11:50:51 -0300

    PostgresGrupoDAO try-catchs

--- SyStock/AccesoDatos/PostgreSQL/PostgresGrupoDAO.cs
+++ SyStock/AccesoDatos/PostgreSQL/PostgresGrupoDAO.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Npgsql;
 using SyStock.Entidades;
 using System.Data;
@@ -16,6 +13,7 @@ namespace SyStock.AccesoDatos.PostgreSQL
     {
         private readonly NpgsqlConnection _conexion;
 
+        
         public PostgresGrupoDAO(NpgsqlConnection pConexion)
         {
             _conexion = pConexion;
@@ -34,7 +32,7 @@ namespace SyStock.AccesoDatos.PostgreSQL
 
             try
             {
-                NpgsqlCommand comando = this._conexion.CreateCommand();
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
 
                 comando.CommandText = query;
                 comando.Parameters.AddWithValue("@nombre", pGrupo.Nombre);
@@ -62,18 +60,28 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <returns></returns>
         public int VerificarNombre(string pNombre, int pIdArea)
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
+            string query = "SELECT \"idGrupo\" FROM \"Grupo\" WHERE nombre = '" + pNombre + "' and \"idArea\" = '" + pIdArea + "'";
 
-            comando.CommandText = "SELECT \"idGrupo\" FROM \"Grupo\" WHERE nombre = '" + pNombre + "' and \"idArea\" = '" + pIdArea + "'";
-            NpgsqlDataReader reader = comando.ExecuteReader();
-            if (reader.Read())
+            try
             {
-                return Int32.Parse(reader[0].ToString());
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
+
+                comando.CommandText = query;
+                using (NpgsqlDataReader reader = comando.ExecuteReader())
+                {
+                    if (reader.Read())
+                        return Int32.Parse(reader[0].ToString());
+                    else
+                        return -1;
+                }
             }
-
-            else
+            catch(PostgresException e)
             {
-                return -1;
+                throw new DAOException("Error al verificar el nombre de grupo: " + e.Message);
+            }
+            catch(NpgsqlException e)
+            {
+                throw new DAOException("Error al verificar el nombre de grupo: " + e.Message);
             }
         }
 
@@ -83,21 +91,41 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <param name="pIdGrupo">ID to search by</param>
         public Grupo Obtener(int pIdGrupo)
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
-            comando.CommandText = "SELECT * FROM \"Grupo\" WHERE \"idGrupo\" = '" + pIdGrupo + "'";
-            Grupo _grupo = new Grupo("",true,0,0);
+            string query = "SELECT * FROM \"Grupo\" WHERE \"idGrupo\" = '" + pIdGrupo + "'";
+            Grupo grupo = null;
 
-            using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
+            try
             {
-                DataTable tabla = new DataTable();
-                adaptador.Fill(tabla);
-
-                foreach (DataRow fila in tabla.Rows)
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
+                comando.CommandText = query;
+                
+                using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
                 {
-                    _grupo = new Grupo(Convert.ToInt32(fila["idGrupo"]), Convert.ToString(fila["nombre"]), Convert.ToBoolean(fila["estado"]), Convert.ToInt32(fila["idArea"]), Convert.ToInt32(fila["idUsuario"]));
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+
+                    foreach (DataRow fila in tabla.Rows)
+                    {
+                        int _id = Convert.ToInt32(fila["idGrupo"]);
+                        string _nombre = Convert.ToString(fila["nombre"]);
+                        bool _estado = Convert.ToBoolean(fila["estado"]);
+                        int _idArea = Convert.ToInt32(fila["idArea"]);
+                        int _idUsuario = Convert.ToInt32(fila["idUsuario"]);
+
+                        grupo = new Grupo(_id, _nombre, _estado, _idArea, _idUsuario);
+                    }
+                    tabla.Dispose();
                 }
+                return grupo;
             }
-            return _grupo;
+            catch(PostgresException e)
+            {
+                throw new DAOException("Error al intentar obtener el grupo de la base de datos: " + e.Message);
+            }
+            catch(NpgsqlException e)
+            {
+                throw new DAOException("Error al intentar obtener el grupo de la base de datos: " + e.Message);
+            }
         }
 
         /// <summary>
@@ -106,21 +134,41 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <param name="pNombre">name to search by</param>
         public Grupo Obtener(string pNombre)
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
-            comando.CommandText = "SELECT * FROM \"Grupo\" WHERE nombre = '" + pNombre + "'";
-            Grupo _grupo = new Grupo("",true,0,0);
+            string query = "SELECT * FROM \"Grupo\" WHERE nombre = '" + pNombre + "'";
+            Grupo grupo = null;
 
-            using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
+            try
             {
-                DataTable tabla = new DataTable();
-                adaptador.Fill(tabla);
-
-                foreach (DataRow fila in tabla.Rows)
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
+                comando.CommandText = query;
+                
+                using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
                 {
-                    _grupo = new Grupo(Convert.ToInt32(fila["idGrupo"]), Convert.ToString(fila["nombre"]), Convert.ToBoolean(fila["estado"]), Convert.ToInt32(fila["idArea"]), Convert.ToInt32(fila["idUsuario"]));
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+
+                    foreach (DataRow fila in tabla.Rows)
+                    {
+                        int _id = Convert.ToInt32(fila["idGrupo"]);
+                        string _nombre = Convert.ToString(fila["nombre"]);
+                        bool _estado = Convert.ToBoolean(fila["estado"]);
+                        int _idArea = Convert.ToInt32(fila["idArea"]);
+                        int _idUsuario = Convert.ToInt32(fila["idUsuario"]);
+
+                        grupo = new Grupo(_id, _nombre, _estado, _idArea, _idUsuario);
+                    }
+                    tabla.Dispose();
                 }
+                return grupo;
             }
-            return _grupo;
+            catch (PostgresException e)
+            {
+                throw new DAOException("Error al intentar obtener el grupo de la base de datos: " + e.Message);
+            }
+            catch(NpgsqlException e)
+            {
+                throw new DAOException("Error al intentar obtener el grupo de la base de datos: " + e.Message);
+            }
         }
 
         /// <summary>
@@ -129,12 +177,28 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <param name="pGrupo">Grupo object with all filled fields</param>
         public void Modificar(Grupo pGrupo)
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
-            comando.CommandText = "UPDATE \"Grupo\" SET nombre = @nombre WHERE \"idGrupo\" = '" + pGrupo.IdGrupo + "'";
+            if (pGrupo == null)
+                throw new ArgumentNullException(nameof(pGrupo));
 
-            comando.Parameters.AddWithValue("@nombre", pGrupo.Nombre);
-            comando.Parameters.AddWithValue("@estado", pGrupo.Estado);
-            comando.ExecuteNonQuery();
+            string query = "UPDATE \"Grupo\" SET nombre = @nombre WHERE \"idGrupo\" = '" + pGrupo.IdGrupo + "'";
+
+            try
+            {
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
+                comando.CommandText = query;
+
+                comando.Parameters.AddWithValue("@nombre", pGrupo.Nombre);
+                comando.Parameters.AddWithValue("@estado", pGrupo.Estado);
+                comando.ExecuteNonQuery();
+            }
+            catch (PostgresException e)
+            {
+                throw new DAOException("Error al modificar grupo: " + e.Message);
+            }
+            catch(NpgsqlException e)
+            {
+                throw new DAOException("Error al modificar grupo: " + e.Message);
+            }
         }
 
         /// <summary>
@@ -143,21 +207,40 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <returns>A list containing objects of class Grupo</returns>
         public List<Grupo> Listar()
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
-            comando.CommandText = "SELECT * FROM \"Grupo\"";
+            string query = "SELECT * FROM \"Grupo\"";
+            List<Grupo> listaGrupo = new List<Grupo>();
 
-            List<Grupo> _listaGrupo = new List<Grupo>();
-
-            using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
+            try
             {
-                DataTable tabla = new DataTable();
-                adaptador.Fill(tabla);
-                foreach (DataRow fila in tabla.Rows)
+                using NpgsqlCommand comando = this._conexion.CreateCommand();
+                comando.CommandText = query;
+
+                using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
                 {
-                    _listaGrupo.Add(new Grupo(Convert.ToInt32(fila["idGrupo"]), Convert.ToString(fila["nombre"]), Convert.ToBoolean(fila["estado"]), Convert.ToInt32(fila["idArea"]), Convert.ToInt32(fila["idUsuario"])));
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    foreach (DataRow fila in tabla.Rows)
+                    {
+                        int _id = Convert.ToInt32(fila["idGrupo"]);
+                        string _nombre = Convert.ToString(fila["nombre"]);
+                        bool _estado = Convert.ToBoolean(fila["estado"]);
+                        int _idArea = Convert.ToInt32(fila["idArea"]);
+                        int _idUsuario = Convert.ToInt32(fila["idUsuario"]);
+
+                        listaGrupo.Add(new Grupo(_id,_nombre,_estado,_idArea,_idUsuario));
+                    }
+                    tabla.Dispose();
                 }
+                return listaGrupo;
             }
-            return _listaGrupo;
+            catch (PostgresException e)
+            {
+                throw new DAOException("Error al listar grupos: " + e.Message);
+            }
+            catch (NpgsqlException e)
+            {
+                throw new DAOException("Error al listar grupos: " + e.Message);
+            }
         }
 
         /// <summary>
@@ -167,21 +250,40 @@ namespace SyStock.AccesoDatos.PostgreSQL
         /// <returns>A list containing all matching objects</returns>
         public List<Grupo> Listar(int idArea)
         {
-            NpgsqlCommand comando = this._conexion.CreateCommand();
-            comando.CommandText = "SELECT * FROM \"Grupo\" WHERE \"idArea\" = '" + idArea + "'";
+            string query = "SELECT * FROM \"Grupo\" WHERE \"idArea\" = '" + idArea + "'";
+            List<Grupo> listaGrupo = new List<Grupo>();
 
-            List<Grupo> _listaGrupo = new List<Grupo>();
-
-            using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
+            try
             {
-                DataTable tabla = new DataTable();
-                adaptador.Fill(tabla);
-                foreach (DataRow fila in tabla.Rows)
+                NpgsqlCommand comando = this._conexion.CreateCommand();
+                comando.CommandText = query;
+
+                using (NpgsqlDataAdapter adaptador = new NpgsqlDataAdapter(comando))
                 {
-                    _listaGrupo.Add(new Grupo(Convert.ToInt32(fila["idGrupo"]), Convert.ToString(fila["nombre"]), Convert.ToBoolean(fila["estado"]), Convert.ToInt32(fila["idArea"]), Convert.ToInt32(fila["idUsuario"])));
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    foreach (DataRow fila in tabla.Rows)
+                    {
+                        int _id = Convert.ToInt32(fila["idGrupo"]);
+                        string _nombre = Convert.ToString(fila["nombre"]);
+                        bool _estado = Convert.ToBoolean(fila["estado"]);
+                        int _idArea = Convert.ToInt32(fila["idArea"]);
+                        int _idUsuario = Convert.ToInt32(fila["idUsuario"]);
+
+                        listaGrupo.Add(new Grupo(_id, _nombre, _estado, _idArea, _idUsuario));
+                    }
+                    tabla.Dispose();
                 }
+                return listaGrupo;
             }
-            return _listaGrupo;
+            catch (PostgresException e)
+            {
+                throw new DAOException("Error al listar grupos: " + e.Message);
+            }
+            catch(NpgsqlException e)
+            {
+                throw new DAOException("Error al listar grupos: " + e.Message);
+            }
         }
     }
 }
